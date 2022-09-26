6e3990abdc58af543957ea274dc79da74516acdf
cremedelakremlin <michael.leversha@gmail.com>
2022-09-26 14:27:32 +0100

    adds review list functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import { useState } from 'react'
+import ReviewList from './components/ReviewList'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
+    <NavBar />
+    <ReviewList />
+    <Routes>
+        <Route path='/' element={<p>PLACEHOLDER TEXT</p>} />
+        <Route path='/reviews' element={<ReviewList />} />
+        <Route path='*' element={<p>404 not found!</p>} />
+      </Routes>
+   </div>
   );
 }
 
