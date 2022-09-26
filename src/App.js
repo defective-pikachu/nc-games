46850ca08fb7bd36f2e333f9645e972778558fe3
cremedelakremlin <michael.leversha@gmail.com>
2022-09-26 16:50:58 +0100

    adds individual review functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import { useState } from 'react'
 import ReviewList from './components/ReviewList'
 import NavBar from './components/NavBar';
+import ReviewCard from './components/ReviewCard';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/reviews' element={<ReviewList />} />
         <Route path='*' element={<p>404 not found!</p>} />
         <Route path='/categories/:category' element={<ReviewList />} />
+        <Route path='/reviews/:review_id' element={<ReviewCard />} />
       </Routes>
    </div>
   );
