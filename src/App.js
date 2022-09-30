e0b9be2b1e71eb1ce83ebe4eb9649cc58b85bdb1
cremedelakremlin <michael.leversha@gmail.com>
2022-09-30 12:14:20 +0100

    adds error handling 404 pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Routes>
         <Route path='/' element={<ReviewList />} />
         <Route path='/reviews' element={<ReviewList />} />
-        <Route path='*' element={<p className='fourOhFour'>404 not found! <br></br><br></br><img src='https://res.cloudinary.com/practicaldev/image/fetch/s--MDGh9sFX--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/94318/6f20f70e-671b-4f70-9a30-008803d85e48.png' alt='a friendly wombat tells you that you have encountered a 404 error'></img></p> } />
+        <Route path='*' element={<p className='fourOhFour'>404 - page not found! <br></br><br></br><img src='https://res.cloudinary.com/practicaldev/image/fetch/s--MDGh9sFX--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/94318/6f20f70e-671b-4f70-9a30-008803d85e48.png' alt='a friendly wombat tells you that you have encountered a 404 error'></img></p> } />
         <Route path='/categories/:category' element={<ReviewList />} />
         <Route path='/reviews/:review_id' element={<ReviewCard />} />
       </Routes>
